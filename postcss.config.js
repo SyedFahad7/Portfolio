4466beebecbf97a2a2a721615cb6afa51ef67550
Your Name <you@example.com>
2024-07-04 20:08:30 +0530

    Cname

--- postcss.config.js
+++ postcss.config.js
@@ -2,19 +2,20 @@ const path = require('path');
 
 module.exports = {
     plugins: [
-        require("tailwindcss/nesting"),
-        require("tailwindcss"),
-        require("autoprefixer"),
-        ...(process.env.NODE_ENV === "production"
+        require('tailwindcss/nesting'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+        ...(process.env.NODE_ENV === 'production'
             ? [
-                require("cssnano")({
-                    preset: "default",
+                require('cssnano')({
+                    preset: 'default',
                 }),
             ]
             : []),
     ],
+    input: 'assets/styles/main.css',  // Adjust path as per your restored structure
     output: {
         path: path.resolve(__dirname, 'build/assets/styles/'),
-        filename: 'main.min.css'
-    }
+        filename: 'main.min.css',
+    },
 };
